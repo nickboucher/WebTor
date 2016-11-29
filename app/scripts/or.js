10fb7c03ad39a5afe456726431c003438d532c5b
RAPHAEL PESTOURIE <raphaelpestourie@MacBook-Pro-3.local>
2016-11-29 01:37:07 -0500

    comments on how constructor and relay nodes should work

--- app/scripts/or.js
+++ app/scripts/or.js
@@ -22,7 +22,7 @@ export var peers = {};
  * circuit abstraction.
  */
 export class Circuit {
-	/** 
+	/**
 	 * This constructor is equivalent to the TOR create() method, which
 	 * creates a new circuit from a given endpoint. If the previous
 	 * endpoint is unspecified, then this is the entrypoint of the circuit.
@@ -36,6 +36,44 @@ export class Circuit {
 	 * If the post connection is dropped, it will instead send a TRUNCATED message
 	 * to the prior connection and remain open.
 	 */
+
+
+
+/*
+node that creates a circuit has:
+- list of the ip address of the circuit
+- list of symmetric key to communicate with
+- circid for the connection with the next node
+(-onion key g? of the next node)
+the construction will
+- send create to the next node to set up symmetric key by deffie-hellman
+- send create embedded in relay_extend commands (symmetrically encrypted)
+- send something to do to the last node of the circuit by encrypting in reverse order
+the truncation will
+- send truncate embedded in relays
+-update lists
+the destroy will
+- send a destroy to the next
+- delete the lists
+if:
+- receives a created -> continue building circuit
+- receives a relay -> decrypt in order */
+
+/*
+relay node has:
+- hash table of the circId incoming and outcoming, and symmetric key with incoming
+this node will do
+- if receives a create, set the symmetric key + send created back
+- if receives an extend then create a new connection with the next node
+- if receives a relay: if it is from incoming side then decrypt and change circId
+if it is from outcoming side then encrypt and change circid
+- if receives destroy, send destroy to next with next circid and delete entry in table
+- if receives a truncate, send destroy to next with correct circid, and becomes an exit node */
+
+/*
+shall we further discuss exit nodes?
+/*
+
 	constructor() {
 	}
 
@@ -74,7 +112,7 @@ export class Circuit {
 	}
 }
 
-/** 
+/**
  * This default export object contains all the global functions
  */
 export default {
